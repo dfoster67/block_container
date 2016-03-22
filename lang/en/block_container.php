7169ab5603bcda26ec04b017e460d710c6e68a17
dfoster67 <doug.foster@intersystems.com>
2016-03-22 15:13:35 -0400

    Initial files
    Added initial core files for block

---
+++ lang/en/block_container.php
@@ -0,0 +1,5 @@
+<?php
+$string['pluginname'] = 'Container Block';
+$string['simplehtml'] = 'Continer Block';
+$string['simplehtml:addinstance'] = 'Add a new linux container block';
+$string['simplehtml:myaddinstance'] = 'Add a new linux container block to the My Moodle page';
